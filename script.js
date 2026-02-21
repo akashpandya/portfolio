6a1708fc20dbf893d25e1c6fcdb0a15f34247c23
Akash A Pandya <91130000+akashpandya@users.noreply.github.com>
2026-02-21 17:49:44 -0500

    Update script.js
    First Update

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+
+  const isOpen = menu.classList.toggle("open");
+  icon.classList.toggle("open");
+
+  // Accessibility signal for screen readers
+  icon.setAttribute("aria-expanded", isOpen);
 }
